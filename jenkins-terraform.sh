b65d5e788414f50b7db66421c79b784c05d5cf09
saket085 <87834511+saket085@users.noreply.github.com>
2022-04-07 23:06:43 +0100

    Update jenkins-terraform.sh

--- jenkins-terraform.sh
+++ jenkins-terraform.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ex
 AWS_REGION="eu-west-2"
+export PACKER_LOG=1
+export PACKER_LOG_PATH="packerlog.txt"
 ARTIFACT=`packer build -machine-readable packer-demo.json | awk -F, '$0 ~/artifact,0,id/ {print $6}'`
 echo "packer output:"
 cat packer-demo.json
