82e674369efd0a6e476e05fe4dc33e3be881cefc
saket085 <87834511+saket085@users.noreply.github.com>
2022-04-07 21:43:51 +0100

    Update jenkins-terraform.sh

--- jenkins-terraform.sh
+++ jenkins-terraform.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 set -ex
-sudo visudo
 AWS_REGION="eu-west-2"
-
 ARTIFACT=`packer build -machine-readable packer-demo.json | awk -F, '$0 ~/artifact,0,id/ {print $6}'`
 echo "packer output:"
 cat packer-demo.json
-
 AMI_ID=`echo $ARTIFACT | cut -d ':' -f2`
 echo "AMI ID: ${AMI_ID}"
-
 echo "writing amivar.tf and uploading it to s3"
 echo 'variable "APP_INSTANCE_AMI" { default = "'${AMI_ID}'" }' > amivar.tf
 S3_BUCKET=`aws s3 ls --region $AWS_REGION |grep terraform-state |tail -n1 |cut -d ' ' -f3`
